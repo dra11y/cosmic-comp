437bf303bb70302720a4d6cb7348293f9cdaa748
Tom Grushka <tom@dra11y.com>
2026-07-04 00:41:17 -0600

    multiply instead of add zoom factor

--- src/input/actions.rs
+++ src/input/actions.rs
@@ -1101,7 +1101,13 @@ impl State {
         }
 
         if zoom_seat == *seat {
-            let new_level = (current_level + change).max(1.0);
+            let factor = 1.0 + change.abs();
+            let new_level = if change >= 0.0 {
+                (current_level * factor).max(1.0)
+            } else {
+                (current_level / factor).max(1.0)
+            };
+
             shell.trigger_zoom(
                 seat,
                 Some(&output),
