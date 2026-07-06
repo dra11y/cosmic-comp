53cccdd58580a3b07d5c5ab1b629c8094f8012f6
Tom Grushka <tom@dra11y.com>
2026-07-06 00:39:39 -0600

    consolidate ZoomState::animating_focal_point and ZoomState::animating_level into single lock call in ZoomState::animating_focal_point_and_level; pass focal_point and zoom_level down to backend::render::cursor_elements to avoid extra call; remove ZoomState::current_focal_point

--- src/backend/render/mod.rs
+++ src/backend/render/mod.rs
@@ -469,7 +469,8 @@ pub enum CursorMode {
 pub fn cursor_elements<'a, 'frame, R>(
     renderer: &mut R,
     seats: impl Iterator<Item = &'a Seat<State>>,
-    zoom_state: Option<&ZoomState>,
+    focal_point: Point<f64, Local>,
+    zoom_level: f64,
     theme: &Theme,
     now: Time<Monotonic>,
     output: &Output,
@@ -482,14 +483,6 @@ where
     CosmicMappedRenderElement<R>: RenderElement<R>,
 {
     let scale = output.current_scale().fractional_scale();
-    let (focal_point, zoom_scale) = zoom_state
-        .map(|state| {
-            (
-                state.animating_focal_point(Some(output)).to_local(output),
-                state.animating_level(output),
-            )
-        })
-        .unwrap_or_else(|| ((0., 0.).into(), 1.));
     let mut elements = Vec::new();
 
     for seat in seats {
@@ -506,7 +499,7 @@ where
                     seat,
                     location,
                     scale.into(),
-                    zoom_scale,
+                    zoom_level,
                     now,
                     mode != CursorMode::NotDefault,
                 )
@@ -522,7 +515,7 @@ where
                             .as_logical()
                             .to_physical(output.current_scale().fractional_scale())
                             .to_i32_round(),
-                        zoom_scale,
+                        zoom_level,
                     ))
                 }),
             );
@@ -558,7 +551,7 @@ where
                         .as_logical()
                         .to_physical(output.current_scale().fractional_scale())
                         .to_i32_round(),
-                    zoom_scale,
+                    zoom_level,
                 ))
             }));
         }
@@ -588,7 +581,7 @@ where
                     } else {
                         Point::from((0, 0))
                     },
-                    if should_scale { zoom_scale } else { 1.0 },
+                    if should_scale { zoom_level } else { 1.0 },
                 ))
             }));
         }
@@ -706,7 +699,7 @@ pub fn workspace_elements<R>(
     _gpu: Option<&DrmNode>,
     renderer: &mut R,
     shell: &Arc<parking_lot::RwLock<Shell>>,
-    zoom_level: Option<&ZoomState>,
+    zoom_state: Option<&ZoomState>,
     now: Time<Monotonic>,
     output: &Output,
     previous: Option<(WorkspaceHandle, usize, WorkspaceDelta)>,
@@ -734,9 +727,14 @@ where
     // that is prone to deadlock with the main-thread on some grabs.
     std::mem::drop(shell_ref);
 
+    let (focal_point, zoom_level) = zoom_state
+        .map(|state| state.animating_focal_point_and_level(output))
+        .unwrap_or_else(|| ((0., 0.).into(), 1.));
+
     elements.extend(cursor_elements(
         renderer,
         seats.iter(),
+        focal_point,
         zoom_level,
         &theme,
         now,
@@ -792,14 +790,6 @@ where
         .size
         .as_logical()
         .to_physical_precise_round(scale);
-    let (focal_point, zoom_scale) = zoom_level
-        .map(|state| {
-            (
-                state.animating_focal_point(Some(output)).to_local(output),
-                state.animating_level(output),
-            )
-        })
-        .unwrap_or_else(|| ((0., 0.).into(), 1.));
 
     let crop_to_output = |element: WorkspaceRenderElement<R>| {
         CropRenderElement::from_element(
@@ -809,7 +799,7 @@ where
                     .as_logical()
                     .to_physical(output.current_scale().fractional_scale())
                     .to_i32_round(),
-                zoom_scale,
+                zoom_level,
             ),
             scale,
             Rectangle::from_size(output_size),
@@ -1484,7 +1474,7 @@ pub fn render_workspace<'d, R>(
     age: usize,
     additional_damage: Option<Vec<Rectangle<i32, Logical>>>,
     shell: &Arc<parking_lot::RwLock<Shell>>,
-    zoom_level: Option<&ZoomState>,
+    zoom_state: Option<&ZoomState>,
     now: Time<Monotonic>,
     output: &Output,
     previous: Option<(WorkspaceHandle, usize, WorkspaceDelta)>,
@@ -1504,7 +1494,7 @@ where
         gpu,
         renderer,
         shell,
-        zoom_level,
+        zoom_state,
         now,
         output,
         previous,
